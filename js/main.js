18bae18a560ad246b9ad1f1d64ca8161fc4a6cd2
Aaron Long <J.AaronLong@gmail.com>
2021-03-05 19:17:15 -0800

    Add basic css style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 /*----- constants -----*/
 const MAX_GUESSES = 10;
-const WORDS = [];
+const WORDS = [
+  "TEST",
+  "ONE",
+  "TWO",
+  "THREE",
+  "FOUR",
+  "FIVE",
+  "SIX",
+  "SEVEN",
+  "EIGHT",
+  "NINE",
+  "TEN",
+  "ELEVEN",
+  "TWELVE",
+];
 /*----- app's state (variables) -----*/
 let board, hiddenWord, guesses, incorrectLetters;
 
 /*----- cached element references -----*/
+const replayBtn = document.getElementById("reset");
+const msgEl = document.getElementById("guess-msg");
 
 /*----- event listeners -----*/
-
+replayBtn.addEventListener("click", init);
 /*----- functions -----*/
+
+init();
+
+function init() {
+  console.log(`It's working!`);
+}
+
+function render() {}
