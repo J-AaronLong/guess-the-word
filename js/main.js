5e7ec9fc09046de1921233778257403829fce000
Aaron Long <J.AaronLong@gmail.com>
2021-03-09 12:16:31 -0800

    add more styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 /*----- constants -----*/
 const MAX_GUESSES = 10;
 const WORDS = [
-  "TEST",
   "ONE",
   "TWO",
   "THREE",
@@ -11,25 +10,43 @@ const WORDS = [
   "SEVEN",
   "EIGHT",
   "NINE",
-  "TEN",
-  "ELEVEN",
-  "TWELVE",
+  "TEST TEST",
 ];
 /*----- app's state (variables) -----*/
-let board, hiddenWord, guesses, incorrectLetters;
+let hiddenWord, guesses, wrongLetters;
 
 /*----- cached element references -----*/
 const replayBtn = document.getElementById("reset");
 const msgEl = document.getElementById("guess-msg");
+const gameWord = document.getElementById("board");
 
 /*----- event listeners -----*/
 replayBtn.addEventListener("click", init);
+// gameWord.addEventListenever("onkeypress", keyLog);
+
 /*----- functions -----*/
 
 init();
 
 function init() {
-  console.log(`It's working!`);
+  // Random word from array
+  hiddenWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+  console.log(hiddenWord);
+
+  guesses = 10;
+
+  wrongLetters = [];
+
+  render();
 }
 
-function render() {}
+function render() {
+  console.log("Render");
+}
+
+// function keyLog() {
+//   console.log("keyLog", gameWord);
+// }
+
+// Notes as I go:
+// Looking into differences between onkeypress, keydown, keyup and trying to figure out ways to render them to my "board", I came to the conclusion that this might not be a very responsive way to build the game as a tablet user wouldn't be able to play. So I may need to rethink how to approach the letter inputs. Look into options to bring up mobile or table keyboard?
